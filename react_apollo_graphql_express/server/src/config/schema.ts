e9851c0187e1812727e6cad8ddc471527cae239f
hoo00nn <hoo00nn@naver.com>
2020-12-05 17:28:07 +0900

    Refactor #noissue : prettier 적용

--- react_apollo_graphql_express/server/src/config/schema.ts
+++ react_apollo_graphql_express/server/src/config/schema.ts
@@ -1,18 +1,11 @@
-import {
-  makeExecutableSchema,
-  mergeTypeDefs,
-  mergeResolvers,
-  loadFilesSync,
-} from "graphql-tools";
-import path from "path";
+import { makeExecutableSchema, mergeTypeDefs, mergeResolvers, loadFilesSync } from 'graphql-tools';
+import path from 'path';
 
 // api 폴더 내의 graphql 확장자를 가진 파일을 배열 형태로 가져온다.
-const allTypes = loadFilesSync(path.join(__dirname, "../api/**/*.graphql"));
+const allTypes = loadFilesSync(path.join(__dirname, '../api/**/*.graphql'));
 
 // api 폴더 내의 resolver.ts 확장자를 가진 파일을 배열 형태로 가져온다.
-const allResolvers = loadFilesSync(
-  path.join(__dirname, "../api/**/*.resolvers.ts")
-);
+const allResolvers = loadFilesSync(path.join(__dirname, '../api/**/*.resolvers.ts'));
 
 const schema = makeExecutableSchema({
   typeDefs: mergeTypeDefs(allTypes), // 가져온 graphql 파일의 타입을 합쳐준다.
