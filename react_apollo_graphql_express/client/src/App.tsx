8ef8a371f878eb9b5bfa2da8fcc6db6cff6381f2
hoo00nn <hoo00nn@naver.com>
2020-12-06 12:04:20 +0900

    Chore #noissue : Apollo 설정

--- react_apollo_graphql_express/client/src/App.tsx
+++ react_apollo_graphql_express/client/src/App.tsx
@@ -1,7 +1,14 @@
 import React, { FC } from 'react';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+import apollo from './apollo';
 
 const App: FC = () => {
-  return <></>;
+  return (
+    <ApolloProvider client={apollo}>
+      <span>hello</span>
+    </ApolloProvider>
+  );
 };
 
 export default App;
